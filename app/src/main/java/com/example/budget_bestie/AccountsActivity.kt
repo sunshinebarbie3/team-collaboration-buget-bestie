2ff22e04bbb69471987b409f0104285d5ed71825
lizzy molokomme <ST10439626@rcconnect.edu.za>
2026-04-26 23:12:52 +0200

    third phase

--- app/src/main/java/com/example/budget_bestie/AccountsActivity.kt
+++ app/src/main/java/com/example/budget_bestie/AccountsActivity.kt
@@ -1,20 +1,28 @@
-package com.budgetbestie
+package com.budgetbestie.activities
 
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.budgetbestie.R
 
 class AccountsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_accounts)
+        setContentView(R.layout.activity_accounts_screen)
 
         supportActionBar?.title = "Accounts"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val addAccountButton = findViewById<Button>(R.id.buttonAddAccount)
-        addAccountButton.setOn
+        addAccountButton.setOnClickListener {
+            Toast.makeText(this, "Add Account feature coming soon!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
